19da00c5ac628bac83c2a9441163bf7ebcb23076
rmdevjr <63247511+rmdevjr@users.noreply.github.com>
2020-07-07 09:59:22 -0300

    currency converter component creation, component development, component styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Conversor from "./component/Conversor"
 
 function App() {
+  
   return (
+    
+    
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Conversor De Moedas</h1>
+      <hr/>
+      <div className="linha1">
+        <Conversor  moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor  moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className="linha2">
+        <Conversor  moedaA="CAD" moedaB="BRL"></Conversor>
+        <Conversor  moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+      <div className="linha3">
+        <Conversor  moedaA="EUR" moedaB="BRL"></Conversor>
+        <Conversor  moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
     </div>
   );
 }
